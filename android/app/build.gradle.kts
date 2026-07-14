ebb1174fe78e4c98c29c6528f86f1aef483e09ad
mrcong <zh-lei@foxmail.com>
2026-07-14 12:55:50 +0800

    fix: enable core library desugaring for Android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -49,4 +50,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
 }
